c6fcdf2f82d92ba646571e40b30b865a46bcfce8
altinukshini <altin.ukshini@gmail.com>
2018-05-08 10:40:22 +0200

    Remove cmnts

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ let app = express()
 let server = require('http').Server(app)
 let io = module.exports.io = require('socket.io')(server)
 
-// Use the environment port if available, or default to 3000
 let port = process.env.PORT || 3001
 
-// Serve static files from /public
 app.use(express.static('public'))
-
-// Create an endpoint which just returns the index.html page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 const SocketManager = require('./SocketManager')
